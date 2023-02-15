49da12e1b0a853c1020c833a6b20b06e1ba50768
Marko <61699047+Marrkkov@users.noreply.github.com>
2023-02-15 11:01:42 -0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-      System.out.println("Trying Webhook third strike try!!!");
+      System.out.println("Trying Webhook retest!!!");
   }
 }
